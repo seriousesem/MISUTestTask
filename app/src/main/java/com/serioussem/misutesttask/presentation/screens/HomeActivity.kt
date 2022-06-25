6b8a8d46e3a4687bd59f8fdc94aaf6f47f36486f
iSem <semenenko.andrej@gmail.com>
2022-06-25 12:58:11 +0300

    Initial commit

---
+++ app/src/main/java/com/serioussem/misutesttask/presentation/screens/HomeActivity.kt
@@ -0,0 +1,12 @@
+package com.serioussem.misutesttask.presentation.screens
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.serioussem.misutesttask.R
+
+class HomeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.home_activity)
+    }
+}
